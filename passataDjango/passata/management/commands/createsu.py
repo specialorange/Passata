838b2f95a5abccca6b074272e9ab61ab8e5d3f9f
Chris Frisina <sourcetree@specialorange.org>
2022-06-20 02:02:13 -0400

    clarifying user model

--- passataDjango/passata/management/commands/createsu.py
+++ passataDjango/passata/management/commands/createsu.py
@@ -1,13 +1,22 @@
-# images/management/commands/createsu.py
+# passata/management/commands/createsu.py
+import os
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
     help = "Creates a superuser."
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(username="admin", password="thisispublic")
+        if (
+            not get_user_model()
+            .objects.filter(username=os.environ["DJANGO_ADMIN_UNAME"])
+            .exists()
+        ):
+            get_user_model().objects.create_superuser(
+                username=os.environ["DJANGO_ADMIN_UNAME"],
+                password=os.environ["DJANGO_ADMIN_PW"],
+            )
         print("Superuser has been created.")

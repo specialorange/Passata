471e9a601658f7bb91dffa208f2b1853a30bc459
Chris Frisina <sourcetree@specialorange.org>
2022-06-21 22:13:47 -0400

    gunicorn

--- passataDjango/passata/gunicorn.sh
+++ passataDjango/passata/gunicorn.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-NAME="Gunicorn-Server"                                     # Name of the application
+NAME="Gunicorn-Server"                                           # Name of the application
 DJANGODIR=/home/ubuntu/passataDjango                             # Django project directory
-SOCKFILE=/home/ubuntu/passataDjango/passata/wsgi.sock # we will communicate using this unix socket
-USER=ubuntu                                             # the user to run as
-GROUP=ubuntu                                           # the group to run as
-NUM_WORKERS=2                                           # how many worker processes should Gunicorn spawn
+SOCKFILE=/home/ubuntu/passataDjango/passata/wsgi.sock            # we will communicate using this unix socket
+USER=ubuntu                                                      # the user to run as
+GROUP=ubuntu                                                     # the group to run as
+NUM_WORKERS=2                                                    # how many worker processes should Gunicorn spawn
 # timeout for uploads https://stackoverflow.com/questions/6816215/gunicorn-nginx-timeout-problem
-TIMEOUT=300                                           
-DJANGO_SETTINGS_MODULE=passata.settings       # which settings file should Django use
-DJANGO_WSGI_MODULE=passata.wsgi                          # WSGI module name
+TIMEOUT=300
+DJANGO_SETTINGS_MODULE=passata.settings                         # which settings file should Django use
+DJANGO_WSGI_MODULE=passata.wsgi                                 # WSGI module name
 
 echo "Starting $NAME as `whoami` at: `date +%H:%M:%S--%m-%d-%Y`"
 
@@ -33,6 +33,5 @@ exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --timeout=$TIMEOUT \
   --bind=unix:$SOCKFILE \
   --log-level=critical \
-  --log-file=$DJANGODIR/logs/gunicorn.log
+  --log-file=$DJANGODIR/backend/logs/gunicorn.log
   # --error-logfile=$DJANGODIR/logs/gunicorn-error.log \
-
